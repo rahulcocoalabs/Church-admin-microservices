7edbabe035936132414736af478c0b398f42faf3
Rahul <rahul.cocoalabs.com>
2020-08-22 10:38:00 +0530

    change

--- app/controllers/masters.controller.js
+++ app/controllers/masters.controller.js
@@ -11,7 +11,7 @@ exports.churchList = async (req, res) => {
         var projection = {
             name: 1
         };
-        var listChurch = await Church.find(filter,projection).sort({
+        var listChurch = await Church.find(filter, projection).sort({
             'tsCreatedAt': -1
         });
         res.status(200).send({
@@ -76,15 +76,15 @@ exports.parishWardList = async (req, res) => {
     }
 }
 
-exports.eventCategoryList = async(req,res) =>{
-        var filter = {
-            status: 1,
-            status: 1
-        };
-        var projection = {
-            name: 1
-        };
-        var listEventCategoryList = await EventCategory.find(filter, projection)
+exports.eventCategoryList = async (req, res) => {
+    var filter = {
+        status: 1,
+        status: 1
+    };
+    var projection = {
+        name: 1
+    };
+    var listEventCategoryList = await EventCategory.find(filter, projection)
         .sort({
             'tsCreatedAt': -1
         })
@@ -98,11 +98,99 @@ exports.eventCategoryList = async(req,res) =>{
     if (listEventCategoryList && listEventCategoryList.success && (listEventCategoryList.success === 0)) {
         return res.send(listEventCategoryList);
     }
-        
-      return res.status(200).send({
-            success: 1,
-            items: listEventCategoryList,
-            message : 'Event category list'
-        })
 
+    return res.status(200).send({
+        success: 1,
+        items: listEventCategoryList,
+        message: 'Event category list'
+    })
+
+}
+
+exports.getPayementGatewaySettings = async (req, res) => {
+    var identity = req.identity.data;
+    var adminUserId = identity.id;
+    var churchId = identity.church;
+    let churchData = await Church.findOne({
+        _id: churchId,
+        status: 1,
+    }, {
+        paymentGatewayKey: 1
+    })
+        .catch(err => {
+            return {
+                success: 0,
+                message: 'Something went wrong while getting church data',
+                error: err
+            }
+        })
+    if (churchData && churchData.success && (churchData.success === 0)) {
+        return res.send(churchData);
+    }
+    if (churchData) {
+        return res.send({
+            success: 1,
+            paymentGatewayKey: churchData.paymentGatewayKey || "",
+            message: 'Payment gateway details'
+        })
+    } else {
+        return res.send({
+            success: 0,
+            message: 'Church not found'
+        })
+    }
+}
+
+exports.updatePayementGatewaySettings = async (req, res) => {
+    var identity = req.identity.data;
+    var adminUserId = identity.id;
+    var churchId = identity.church;
+    var params = req.body;
+    if (!params.paymentGatewayKey) {
+        return res.send({
+            success: 0,
+            message: 'Nothing to update'
+        })
+    }
+    let findCriteria = {
+        _id: churchId,
+        status: 1,
+    }
+    let churchData = await Church.findOne(findCriteria)
+        .catch(err => {
+            return {
+                success: 0,
+                message: 'Something went wrong while getting church data',
+                error: err
+            }
+        })
+    if (churchData && churchData.success && (churchData.success === 0)) {
+        return res.send(churchData);
+    }
+    if (churchData) {
+        let update = {};
+        update.paymentGatewayKey = params.paymentGatewayKey;
+        update.tsModifiedAt = Date.now()
+        let updateData = await Church.updateOne(findCriteria, update)
+            .catch(err => {
+                return {
+                    success: 0,
+                    message: 'Something went wrong while updating payment gateway settings',
+                    error: err
+                }
+            })
+        if (updateData && updateData.success && (updateData.success === 0)) {
+            return res.send(updateData);
+        }
+        return res.send({
+            success: 1,
+            paymentGatewayKey: params.paymentGatewayKey,
+            message: 'Payment gateway updated successfully'
+        })
+    } else {
+        return res.send({
+            success: 0,
+            message: 'Church not found'
+        })
+    }
 }
--- app/routes/masters.routes.js
+++ app/routes/masters.routes.js
@@ -1,7 +1,10 @@
+const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const masters = require('../controllers/masters.controller');
     app.get('/admin/masters/church/list', masters.churchList);
     app.get('/admin/masters/:id/parish/list', masters.parishList);
     app.get('/admin/masters/:id/parish-ward/list', masters.parishWardList);
     app.get('/admin/masters/event-category/list', masters.eventCategoryList);
+    app.get('/admin/masters/payment/settings',auth, masters.getPayementGatewaySettings);
+    app.patch('/admin/masters/payment/settings',auth, masters.updatePayementGatewaySettings);
 };
