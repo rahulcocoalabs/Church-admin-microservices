4970741957cd3ebaee601fc8cace95cf865fe165
Rahul <rahul.cocoalabs.com>
2020-08-28 16:03:10 +0530

    change

--- config/app.config.js
+++ config/app.config.js
@@ -77,7 +77,7 @@ module.exports = {
   },
   oneSignal: {
     appId : "9777ade8-faad-4cc1-96fa-8f3109efa482",
-    apiKey: ":AAAAGBV0me0:APA91bHhvNC6N0gvjLjcsuNDT1uDASIx25mt56gCGkf2VSnDkA6G1iSsW9etroTQIR_F7kqBEWM95jsVFM7U_RqtXlcAskw8JzvkE-CMZLQ6naZZ58zbNhXyJ8JRCRFexR1H9F54luxG",
+    apiKey: "AAAAGBV0me0:APA91bHhvNC6N0gvjLjcsuNDT1uDASIx25mt56gCGkf2VSnDkA6G1iSsW9etroTQIR_F7kqBEWM95jsVFM7U_RqtXlcAskw8JzvkE-CMZLQ6naZZ58zbNhXyJ8JRCRFexR1H9F54luxG",
   },
 
 }
