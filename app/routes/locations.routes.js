7d3eeae95d9338fc1e0d3294448cd44f1e7fafcc
Rahul <rahul.cocoalabs.com>
2020-09-01 14:26:27 +0530

    change

--- app/routes/locations.routes.js
+++ app/routes/locations.routes.js
@@ -5,4 +5,7 @@ module.exports = (app) => {
     const locations = require('../controllers/locations.controller');
     app.get('/admin/locations/list',auth, locations.list);
     app.post('/admin/locations/add',auth, locations.add);
+    app.get('/admin/locations/:id/detail',auth, locations.detail);
+    app.patch('/admin/locations/:id/update',auth, locations.update);
+    app.delete('/admin/locations/:id/delete',auth, locations.delete);
 };
