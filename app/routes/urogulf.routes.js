f1a244632e522e1a964aec71ce314263a4b0f633
Rahul <rahul.cocoalabs.com>
2020-09-04 15:41:18 +0530

    change

--- app/routes/urogulf.routes.js
+++ app/routes/urogulf.routes.js
@@ -3,6 +3,7 @@ const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const urogulf = require('../controllers/urogulf.controller');
     app.get('/admin/urogulf/request/list',auth, urogulf.listRequest);
+    app.get('/admin/urogulf/branch/list',auth, urogulf.listBranch);
     app.post('/admin/urogulf/branch',auth, urogulf.addBranch);
     app.get('/admin/urogulf/branch/:id/detail',auth, urogulf.getBranchDetails);
     app.patch('/admin/urogulf/branch/:id/update',auth, urogulf.updateBranchDetails);
