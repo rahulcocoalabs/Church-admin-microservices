2bcdb3866d3433e8a2952f8ae88d313633d17bb6
Rahul <rahul.cocoalabs.com>
2020-09-14 12:41:51 +0530

    change

--- app/routes/accounts.routes.js
+++ app/routes/accounts.routes.js
@@ -16,8 +16,9 @@ module.exports = (app) => {
     app.post('/admin/accounts/sign-up', accountsValidator.validator('signUp'), accounts.signUp);
     app.post('/admin/accounts/login',accountsValidator.validator('login'), accounts.login);
     app.get('/admin/accounts/donation',auth, accounts.donationList);
-    app.post('/admin/accounts/resetpassword', accounts.reset);
-    app.post('/admin/accounts/addnewpassword', accounts.resetPassword);
+
+    app.post('/admin/accounts/forgot-password', accounts.forgotPassword);
+    app.post('/admin/accounts/reset-password', accounts.resetPassword);
 
     app.get('/admin/accounts/paster-profile',auth, accounts.getPasterProfile);
     app.patch('/admin/accounts/paster-profile',auth,pasterImageUpload.single('image'),  accounts.updatePasterProfile);
